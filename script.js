6fab11083138c180ccc55e507eff8410419f5983
Joao Treto <joaoatreto@gmail.com>
2024-01-17 13:14:22 -0300

    declaring variables

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('form');
+const usernameInput = document.getElementById('username');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const confirmPasswordInput = document.getElementById('password2');
+
