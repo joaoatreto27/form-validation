75be0eca845d9740e9524c474149e812640e2d9f
Joao Treto <joaoatreto@gmail.com>
2024-01-17 13:24:57 -0300

    Adding emial validation

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 const confirmPasswordInput = document.getElementById('password2');
 
+function showInputError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-input error animate__animated animate__headShake';
+    const small = formControl.querySelector('small');
+    small.innerHTML = message;
+    setTimeout( function () {
+        formControl.className = 'form-input error';
+    }, 500);
+}
+
+function showInputSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-input success animate__animated animate__bounceIn ';
+}
+
+function checkEmailValidity(input) {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (regex.test(input.value)) {
+        showInputSuccess(input);
+    }else{
+        showInputError(input, 'EMail is not valid')
+    }
+}
